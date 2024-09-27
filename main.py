1117a6f253efa20639a87b7cb8a4fa2c02c0460f
rhit-schielaj <157640971+rhit-schielaj@users.noreply.github.com>
2024-09-27 14:53:32 -0400

    LinearReg

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import sys
 import numpy
-import pandas
-print(sys.path)
+import pandas as pd
+import requests
+
+
+df = pd.read_csv('data/Advertising.csv')
+
+#Given a dataframe, df, and title of x column and y column,
+#computes linear regression formula
+def LinearReg(df, x, y):
+#df['cost'] = df['TV'] + df['radio'] + df['newspaper']
+#df = df.sort_values(by='cost', ascending=True)
+    xbar = df[x].mean()
+    ybar = df[y].mean()
+    b1Top = 0
+    b1Bot = 0
+    for index, example in df.iterrows():
+        b1Top += (example[x]-xbar)*(example[y]-ybar)
+        b1Bot += (example[x] - xbar)**2
+    b1 = b1Top/b1Bot
+    b0 = ybar - b1*xbar
+    return str("y = "+str(b1)+"x + "+str(b0))
+#print(LinearReg(df, 'TV', 'sales'))
